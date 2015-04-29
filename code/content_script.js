c0594623b02e6d890dcf451ae8792c4800df4f97
Anders Kofoed <anders.kfd@gmail.com>
2015-04-29 18:14:55 +0200

    added content script to appendix

---
+++ code/content_script.js
@@ -0,0 +1,31 @@
+window.onload = setTimeout(updateUrl(),500);
+var passwords = getPwdInputs(); 
+var pwField = passwords[0];
+
+if(pwField){
+    pwField.addEventListener('input', function (callback){
+
+        console.log("input content: " + pwField.value.length);
+        chrome.runtime.sendMessage({pwLength: pwField.value.length}, function(respons){
+            console.log("Respons pw changed: " + respons);
+        });
+        
+    });
+}
+
+function getPwdInputs() {
+      var ary = [];
+        var inputs = document.getElementsByTagName("input");
+        for (var i=0; i<inputs.length; i++) {
+            if (inputs[i].type.toLowerCase() === "password") {
+                      ary.push(inputs[i]);
+            }
+        }
+          return ary;
+}
+
+function updateUrl(){
+    chrome.runtime.sendMessage({newUrl: window.location.hostname},function(respons){
+        console.log("URL changed to: "+ window.location.hostname);
+    });
+}
